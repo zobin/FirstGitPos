9d97d7cb17e973c22ff7928c25f8b0ae997eb9c7
zobin <i.zhangbin@qq.com>
2019-11-13 14:31:13 +0800

    edit on branch 'master'

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#text 
 echo "git external command"
 
 echo "line 2"
@@ -8,4 +8,7 @@ echo "master-branch-2 1"
 	|___"master-branch-2-b1 1"
 	|
 	|___"master-branch-2-b2 1"
+	|
+	|___"edit on branch 'master'"
+	
 	 
